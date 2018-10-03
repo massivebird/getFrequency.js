f8b3f6f0d0e639558ffa23c1336edf1084ce0213
parrotflakes <gdrakemail@gmail.com>
2018-10-02 21:17:35 -0400

    Minor format change, VSCode won't stop bugging me

--- function.getFrequency.js
+++ function.getFrequency.js
@@ -2,27 +2,27 @@
 // • Does not return anything in the case of letters that do not appear within string (I'm proud of that one)
 
 function getFrequency(string) {
-    console.log(`In the string "${string.toUpperCase()}:"`)
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    for (let i = 0; i < 26; i++) { // Runs next loop for each letter of alphabet
-      let letterAlpha = alphabet.charAt(i);
-      let letterFrequency = 0; // Resets letterFrequency
-      for (let j = 0; j <= string.length; j++) { // Searches string for same letter as i
-        let letterString = string.charAt(j);
-        if (letterAlpha === letterString) { // If letters match
-          letterFrequency ++ // ... add 1 to the frequency
-        } else if (j === string.length) { // If the last letter in string has been searched
-          if (letterFrequency > 0) { // ... and letterFrequency is greater than zero
-            console.log(`The letter ${letterAlpha} appears ${letterFrequency} times.`) // print frequency
-          } else {
-            break; // Go back to first 'for' loop
-          }
+  console.log(`In the string "${string.toUpperCase()}:"`)
+  const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+  for (let i = 0; i < 26; i++) { // Runs next loop for each letter of alphabet
+    let letterAlpha = alphabet.charAt(i);
+    let letterFrequency = 0; // Resets letterFrequency
+    for (let j = 0; j <= string.length; j++) { // Searches string for same letter as i
+      let letterString = string.charAt(j);
+      if (letterAlpha === letterString) { // If letters match
+        letterFrequency++ // ... add 1 to the frequency
+      } else if (j === string.length) { // If the last letter in string has been searched
+        if (letterFrequency > 0) { // ... and letterFrequency is greater than zero
+          console.log(`The letter ${letterAlpha} appears ${letterFrequency} times.`) // print frequency
+        } else {
+          break; // Go back to first 'for' loop
         }
       }
     }
   }
-  
-  getFrequency('I want taco bell') 
+}
+
+getFrequency('I want taco bell')
 
     /* RETURNS ...
 
